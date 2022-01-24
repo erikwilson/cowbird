4c92a33622ff771d1446e86d8255b7d82a60c34d
Erik Wilson <Erik.E.Wilson@gmail.com>
2022-01-24 16:15:51 -0700

    Fixed tab size

--- src/file/mod.rs
+++ src/file/mod.rs
@@ -1,15 +1,15 @@
 use crate::log;
 
 lazy_static! {
-  static ref LOGGER: slog::Logger = log::LOGGER.new(o!("type" => "file"));
+    static ref LOGGER: slog::Logger = log::LOGGER.new(o!("type" => "file"));
 }
 
 pub fn copy(source: &String, target: &String) {
-  println!("todo: implement copy: {:?} {:?}", source, target);
-  info!(LOGGER,"copy"; "source" => source, "target" => target);
+    println!("todo: implement copy: {:?} {:?}", source, target);
+    info!(LOGGER,"copy"; "source" => source, "target" => target);
 }
 
 pub fn delete(file: &String) {
-  println!("todo: implement delete: {:?}", file);
-  info!(LOGGER,"delete"; "file" => file);
+    println!("todo: implement delete: {:?}", file);
+    info!(LOGGER,"delete"; "file" => file);
 }
--- src/log.rs
+++ src/log.rs
@@ -4,30 +4,30 @@ use std::io::Write;
 use std::sync::Mutex;
 
 lazy_static! {
-  pub static ref LOGGER: Logger = {
-    let pkg_info = o!("name" => env!["CARGO_PKG_NAME"], "version" => env!("CARGO_PKG_VERSION"));
+    pub static ref LOGGER: Logger = {
+        let pkg_info = o!("name" => env!["CARGO_PKG_NAME"], "version" => env!("CARGO_PKG_VERSION"));
 
-    let output = match &*get_log_file() {
-      "-" => Box::new(std::io::stdout()) as Box<dyn Write + Send>,
-      path => Box::new(
-        std::fs::OpenOptions::new()
-          .create(true)
-          .write(true)
-          .append(true)
-          .open(path)
-          .expect("failed to open log file"),
-      ) as Box<dyn Write + Send>,
+        let output = match &*get_log_file() {
+            "-" => Box::new(std::io::stdout()) as Box<dyn Write + Send>,
+            path => Box::new(
+                std::fs::OpenOptions::new()
+                    .create(true)
+                    .write(true)
+                    .append(true)
+                    .open(path)
+                    .expect("failed to open log file"),
+            ) as Box<dyn Write + Send>,
+        };
+
+        return Logger::root(Mutex::new(Json::default(output)).fuse(), pkg_info);
     };
-
-    return Logger::root(Mutex::new(Json::default(output)).fuse(), pkg_info);
-  };
-  static ref LOG_FILE: Mutex<String> = Mutex::new("-".to_string());
+    static ref LOG_FILE: Mutex<String> = Mutex::new("-".to_string());
 }
 
 pub fn set_log_file(log_file: String) {
-  *LOG_FILE.lock().unwrap() = log_file;
+    *LOG_FILE.lock().unwrap() = log_file;
 }
 
 pub fn get_log_file() -> String {
-  return LOG_FILE.lock().unwrap().clone();
+    return LOG_FILE.lock().unwrap().clone();
 }
